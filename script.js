46828815e92d928c1ce8a26dfa643d9a6f9d223c
Ryan <ryanoliver565@gmail.com>
2024-02-01 11:40:15 +0800

    fix redirect to home

--- script.js
+++ script.js
@@ -4,3 +4,9 @@ function redirect(pageName) {
     var newUrl = currentUrl.href + pageName;
     window.location.href = newUrl;
 }
+
+function home() {
+    var currentUrl = new URL(window.location.href);
+    currentUrl.pathname = '/portfolio/';
+    window.location.href = currentUrl.href;
+}
